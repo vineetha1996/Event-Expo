a3919639e98abc09eb1aeffc681341dde41991a2
Vineetha <vineethay96@gmail.com>
2020-10-10 01:50:32 -0500

    Reset the marging top layout of all the widgets in activity_profile.xml file.

--- app/src/main/java/com/example/event_expo/UserHomeActivity.java
+++ app/src/main/java/com/example/event_expo/UserHomeActivity.java
@@ -1,13 +1,22 @@
 package com.example.event_expo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserHomeActivity extends AppCompatActivity {
+    private Button bookbtn1, bookbtn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home);
+
+        bookbtn1 = (Button) findViewById(R.id.bookNowBTN1);
+        bookbtn2 = (Button) findViewById(R.id.bookNowBTN2);
+
+
     }
 }
