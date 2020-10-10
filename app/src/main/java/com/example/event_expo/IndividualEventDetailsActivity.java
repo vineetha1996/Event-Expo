464bfafd049b297a3b39d51bc7674785ef82af14
annie0sc <annie0sc@gmail.com>
2020-10-09 20:58:34 -0500

    Created an activity for displaying the event details of the tickets that user wants to book.

---
+++ app/src/main/java/com/example/event_expo/IndividualEventDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.example.event_expo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IndividualEventDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual_event_details);
+    }
+}
