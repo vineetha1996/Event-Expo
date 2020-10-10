f6b7800435f1f734ed4bebdca83c8adfc9e965e4
Vineetha <vineethay96@gmail.com>
2020-10-10 00:37:17 -0500

    Added Share buttons in the user home page.

--- app/src/main/java/com/example/event_expo/UserMaintenanceActivity.java
+++ app/src/main/java/com/example/event_expo/UserMaintenanceActivity.java
@@ -1,13 +1,49 @@
 package com.example.event_expo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class UserMaintenanceActivity extends AppCompatActivity {
+
+    TextView homeTV,profileTV,orderHistoryTV,logoutTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_maintenance);
+
+        homeTV = (TextView) findViewById(R.id.adminTV);
+        profileTV = (TextView) findViewById(R.id.registerTV);
+        orderHistoryTV = (TextView) findViewById(R.id.forgotTV);
+        logoutTV = (TextView) findViewById(R.id.forgotTV);
+
+
+        homeTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeIntent = new Intent(UserMaintenanceActivity.this,UserHomeActivity.class);
+                startActivity(homeIntent);
+            }
+        });
+
+        orderHistoryTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent historyIntent = new Intent(UserMaintenanceActivity.this, OrderHistoryActivity.class);
+                startActivity(historyIntent);
+            }
+        });
+
+        logoutTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent logoutIntent = new Intent(UserMaintenanceActivity.this, LoginActivity.class);
+                startActivity(logoutIntent);
+            }
+        });
+
     }
 }
