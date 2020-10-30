ba74a926d8c3e9baf94f2edaa41159d1164b0b72
Vineetha <vineethay96@gmail.com>
2020-10-29 22:59:08 -0500

    I have added login data as user information for sampling data-flow.

--- app/src/main/java/com/example/event_expo/LoginActivity.java
+++ app/src/main/java/com/example/event_expo/LoginActivity.java
@@ -3,15 +3,17 @@ package com.example.event_expo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView adminLoginTV, registerTV, forgotTV;
     private Button loginbtn;
-
+    EditText login_usernameET,login_password_ET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,17 @@ public class LoginActivity extends AppCompatActivity {
         registerTV = (TextView) findViewById(R.id.registerTV);
         loginbtn = (Button) findViewById(R.id.login_btn);
         forgotTV = (TextView) findViewById(R.id.forgotTV);
+
+        login_usernameET=findViewById(R.id.login_usernameET);
+        login_password_ET=findViewById(R.id.login_password_ET);
+
+        Intent intent=getIntent() ;
+             final String   fullname= intent.getStringExtra("USER_name");
+             String  email=intent.getStringExtra("USER_email");
+        String  phn=intent.getStringExtra("USER_phn");
+        final String  pass=intent.getStringExtra("USER_pass");
+
+        Log.d("suma", fullname+email+phn+pass);
 
         adminLoginTV.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,8 +55,19 @@ public class LoginActivity extends AppCompatActivity {
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent loginIntent = new Intent(LoginActivity.this, UserMaintenanceActivity.class);
-                startActivity(loginIntent);
+
+
+               if(fullname.equals(login_usernameET.getText().toString())&& pass.equals(login_password_ET.getText().toString())){
+
+
+                   Intent loginIntent = new Intent(LoginActivity.this, UserHomeActivity.class);
+                   startActivity(loginIntent);
+
+               }
+else{
+
+               }
+
             }
         });
 
