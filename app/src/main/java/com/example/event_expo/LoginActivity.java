44b0fc6e4888ca30279bf1b855d8fbeb9aa61921
vineetha1996 <vineethay96@gmail.com>
2020-10-31 12:39:45 -0500

    By using putExtra() and getExtra(), I have retrieved the data from SignupActivity.java to the LoginActivity.java

--- app/src/main/java/com/example/event_expo/LoginActivity.java
+++ app/src/main/java/com/example/event_expo/LoginActivity.java
@@ -1,21 +1,20 @@
 package com.example.event_expo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView adminLoginTV, registerTV, forgotTV;
     private Button loginbtn;
     EditText login_usernameET,login_password_ET;
-    String username,email,phn,pass,frstname,lastname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +24,17 @@ public class LoginActivity extends AppCompatActivity {
         registerTV = (TextView) findViewById(R.id.registerTV);
         loginbtn = (Button) findViewById(R.id.login_btn);
         forgotTV = (TextView) findViewById(R.id.forgotTV);
+
         login_usernameET=findViewById(R.id.login_usernameET);
         login_password_ET=findViewById(R.id.login_password_ET);
 
-        final Intent intent=getIntent() ;
-            username= intent.getStringExtra("USER_name");
-            email=intent.getStringExtra("USER_email");
-            phn=intent.getStringExtra("USER_phn");
-            pass = intent.getStringExtra("USER_pass");
-            frstname = intent.getStringExtra("USER_Frstname");
-            lastname = intent.getStringExtra("USER_Lastname");
+        Intent intent=getIntent() ;
+        final String   fullname= intent.getStringExtra("USER_name");
+        String  email=intent.getStringExtra("USER_email");
+        String  phn=intent.getStringExtra("USER_phn");
+        final String  pass=intent.getStringExtra("USER_pass");
 
-        Log.d("Login activity", username+frstname+lastname+email+phn+pass);
+        Log.d("suma", fullname+email+phn+pass);
 
         adminLoginTV.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,26 +57,15 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-               if(username.equals(login_usernameET.getText().toString())&& pass.equals(login_password_ET.getText().toString())){
+                if(fullname.equals(login_usernameET.getText().toString())&& pass.equals(login_password_ET.getText().toString())){
 
-
-                   Intent loginIntent = new Intent(LoginActivity.this, UserMaintenanceActivity.class);
-                   loginIntent.putExtra("loname", username);
-                   loginIntent.putExtra("loemail", email);
-                   loginIntent.putExtra("lophone", phn);
-                   loginIntent.putExtra("loFrstname", frstname);
-                   loginIntent.putExtra("loLastname", lastname);
-                   loginIntent.putExtra("lopassword", pass);
-                   Log.d("Login activity", username+frstname+lastname+email+phn+pass);
-
-                   startActivity(loginIntent);
-               }
-        else{
-                   Toast.makeText(getApplicationContext(), "Username and Password are incorrect", Toast.LENGTH_SHORT).show();
-        }
+                    Intent loginIntent = new Intent(LoginActivity.this, UserMaintenanceActivity.class);
+                    startActivity(loginIntent);
+                }
+                else{
+                }
             }
         });
-
         forgotTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
