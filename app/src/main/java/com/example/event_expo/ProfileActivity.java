0df5eb919d2e8e5cd1180111a6a87100ac53647e
alekhyajaddu <alekhyajaddu@gmail.com>
2020-10-31 12:53:25 -0500

    By using putExtra() and getExtra(), retrieved data credentials from the signup page and added new AppCompatActivity for version x instead version v7.

--- app/src/main/java/com/example/event_expo/ProfileActivity.java
+++ app/src/main/java/com/example/event_expo/ProfileActivity.java
@@ -1,7 +1,8 @@
 package com.example.event_expo;
 
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
 
