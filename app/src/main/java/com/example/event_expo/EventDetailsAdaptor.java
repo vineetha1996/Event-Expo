dee45f6683e4e872b05052b5d7f8a28150075021
annie0sc <annie0sc@gmail.com>
2020-10-31 12:47:38 -0500

    Created EventDetailsAdaptor.java which holds the getters and setters for the events.

--- app/src/main/java/com/example/event_expo/EventDetailsAdaptor.java
+++ app/src/main/java/com/example/event_expo/EventDetailsAdaptor.java
@@ -1,12 +1,13 @@
 package com.example.event_expo;
 
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
@@ -16,15 +17,16 @@ public class EventDetailsAdaptor extends RecyclerView.Adapter<EventDetailsAdapto
 
 
     public class EventDetailsViewHolder extends RecyclerView.ViewHolder {
-        public TextView event_name, date, time, category, desc;
+        public TextView event_name, date, time, desc, category;
         public  RelativeLayout relativeLayout;
 
         public EventDetailsViewHolder(View view) {
             super(view);
+
             event_name = (TextView) view.findViewById(R.id.event_name_TV);
             date = (TextView) view.findViewById(R.id.event_date_TV);
             time = (TextView) view.findViewById(R.id.event_time_TV);
-            //category = (TextView) view.findViewById(R.id.event_category_TV);
+            category = (TextView) view.findViewById(R.id.event_cat_TV);
             desc = (TextView) view.findViewById(R.id.event_description_TV);
         }
     }
@@ -52,9 +54,6 @@ public class EventDetailsAdaptor extends RecyclerView.Adapter<EventDetailsAdapto
         holder.time.setText(tv.getTime());
         holder.category.setText(tv.getCategory());
         holder.desc.setText(tv.getDesc());
-        // holder.relativeLayout.setB
-//        Picasso.get().load(tv.getImage()).into(holder.imageView);
-        //holder.imageView.setImageResource(tv.getImage());
     }
 
     @Override
