8a346e497115c3bcd6dbeef67b9faf264f9960bc
vineetha1996 <vineethay96@gmail.com>
2020-10-31 12:41:38 -0500

    By using putExtra(), The data credentials entered in the SignupActivity.java is stored temporarily.

--- app/src/main/java/com/example/event_expo/SignupActivity.java
+++ app/src/main/java/com/example/event_expo/SignupActivity.java
@@ -1,12 +1,13 @@
 package com.example.event_expo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
 
