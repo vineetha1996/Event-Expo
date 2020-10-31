9775969b0824aef93204aed63253003d4f5ea659
vineetha1996 <vineethay96@gmail.com>
2020-10-31 12:43:02 -0500

    Reverted back to the previous code so as to run the recyclerView

--- app/src/main/java/com/example/event_expo/Admin_login_Activity.java
+++ app/src/main/java/com/example/event_expo/Admin_login_Activity.java
@@ -1,11 +1,12 @@
 package com.example.event_expo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Admin_login_Activity extends AppCompatActivity {
     private Button adminLoginbtn;
