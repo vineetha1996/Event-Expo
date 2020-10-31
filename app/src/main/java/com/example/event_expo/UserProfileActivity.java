394fad59fa8450dcf73a7870cbea0f27f74c3a4f
alekhyajaddu <alekhyajaddu@gmail.com>
2020-10-31 12:52:00 -0500

    Added new AppCompatActivity for version x instead version v7.

--- app/src/main/java/com/example/event_expo/UserProfileActivity.java
+++ app/src/main/java/com/example/event_expo/UserProfileActivity.java
@@ -1,13 +1,14 @@
 package com.example.event_expo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class UserProfileActivity extends AppCompatActivity {
     EditText userfirstname,userlastname,mobilenumber,useremail;
