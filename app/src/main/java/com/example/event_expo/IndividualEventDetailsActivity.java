7ac4a8d0576d15fe8992b013855a2a1a8597c3c9
Bhanuprakash Thota <thotabhanu03@gmail.com>
2020-10-31 12:32:26 -0500

    Added new AppCompatActivity for version x instead version v7.

--- app/src/main/java/com/example/event_expo/IndividualEventDetailsActivity.java
+++ app/src/main/java/com/example/event_expo/IndividualEventDetailsActivity.java
@@ -1,10 +1,11 @@
 package com.example.event_expo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class IndividualEventDetailsActivity extends AppCompatActivity {
 
