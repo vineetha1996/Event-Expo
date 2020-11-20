34eb51ecebe6d4c037543bb51fb54c1a52501782
vineetha1996 <vineethay96@gmail.com>
2020-11-20 02:50:09 -0600

    created Constants.java for saving all the constant values used through out the app

---
+++ app/src/main/java/com/example/event_expo/other/Constants.java
@@ -0,0 +1,41 @@
+package com.example.event_expo.other;
+
+public class Constants {
+
+    public static final int MIN_QUANTITY = 1;
+    public static final int MAX_QUANTITY = 20;
+
+    public static final String DATE_FORMAT = "dd-MMM-yyyy";
+
+    public static final String loginType = "loginType";
+
+    public static final String eventType = "eventType";
+    public static final String quantity = "quantity";
+    public static final String kayaking = "Kayaking";
+    public static final String petting = "Petting";
+    public static final String painting = "Painting";
+    public static final String harvesting = "Harvesting";
+    public static final String soccer = "Soccer";
+    public static final String tennis = "Tennis";
+    public static final String USER_DOCUMENT = "user_document";
+    public static final String EVENT_DOCUMENT_ID = "evv_document_id";
+
+    // Firebase Key Constants
+    public static final String admin = "admin";
+    public static final String name = "name";
+    public static final String date = "date";
+    public static final String price = "price";
+    public static final String description = "description";
+    public static final String time = "time";
+    public static final String user = "user";
+    public static final String event = "event";
+    public static final String orderedEvent = "orderedEvent";
+    public static final String requestedEvent = "requestedEvent";
+    public static final String firstName = "firstName";
+    public static final String lastName = "lastName";
+    public static final String mobileNumber = "mobileNumber";
+    public static final String email = "email";
+    public static final String username = "username";
+    public static final String password = "password";
+
+}
