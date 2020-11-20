b6a7499c1bf2577b4c2a3991f399674f520a86fe
BhanuprakashThota <thotabhanu03@gmail.com>
2020-11-20 02:20:45 -0600

    created OnEventClickListener.java

---
+++ app/src/main/java/com/example/event_expo/listener/OnEventClickListener.java
@@ -0,0 +1,7 @@
+package com.example.event_expo.listener;
+
+import com.example.event_expo.model.Event;
+
+public interface OnEventClickListener {
+    void onEventClick(Event event,int position);
+}
