569e314f44c2c3795d0666931ba05dfdb2987369
alekhyajaddu <alekhyajaddu@gmail.com>
2020-11-20 00:25:24 -0600

    added validations to the username and password fields so that the app would not crash when null values are entered instead it displays a toast message.

--- app/src/main/java/com/example/event_expo/Admin_login_Activity.java
+++ app/src/main/java/com/example/event_expo/Admin_login_Activity.java
@@ -6,32 +6,87 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.event_expo.other.Constants;
+import com.example.event_expo.other.SharedPref;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class Admin_login_Activity extends AppCompatActivity {
+
     private Button adminLoginbtn;
-    EditText adUser,adPass;
-    String adminUser,adminPassword;
+    private EditText adUser, adPass;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login_);
+
+        db = FirebaseFirestore.getInstance();
+
         adUser = findViewById(R.id.admin_username_ET);
         adPass = findViewById(R.id.admin_password_ET);
 
-        adminUser = "Vineethavinni@96";
-        adminPassword = "Vinni@96";
-
-        adminLoginbtn=findViewById(R.id.admin_login_btn);
+        adminLoginbtn = findViewById(R.id.admin_login_btn);
         adminLoginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(adminUser.equals(adUser.getText().toString())&& adminPassword.equals(adPass.getText().toString())){
-                    Intent adminIntent = new Intent(Admin_login_Activity.this,Admin_home_page_Activity.class);
-                    startActivity(adminIntent);
-                }
+                String username = adUser.getText().toString();
+                String password = adPass.getText().toString();
+
+                if (!validateInput(username,password)) return;
+
+                loginAdmin(username,password);
+
             }
         });
     }
+
+    private boolean validateInput(String username, String password) {
+
+        if (username.isEmpty()){
+            Toast.makeText(this, getString(R.string.username_cannot_empty), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (password.isEmpty()){
+            Toast.makeText(this, getString(R.string.password_cannot_empty), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private void loginAdmin(String username, String password) {
+        db.collection(Constants.admin)
+                .whereEqualTo(Constants.username, username)
+                .whereEqualTo(Constants.password, password)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            int size = task.getResult().getDocuments().size();
+                            if (size==1){
+                                SharedPref sharedPref= SharedPref.getInstance();
+                                sharedPref.setLoginType(Admin_login_Activity.this,Constants.admin);
+                                Intent intent = new Intent(Admin_login_Activity.this,Admin_home_page_Activity.class);
+                                startActivity(intent);
+                                finish();
+                            }else if (size==0){
+                                Toast.makeText(Admin_login_Activity.this, getString(R.string.wrong_credentials), Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(Admin_login_Activity.this, getString(R.string.please_try_again), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
 }
