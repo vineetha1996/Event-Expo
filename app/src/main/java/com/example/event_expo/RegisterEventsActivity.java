33e8124000ccac6843f33b4b77a9deae585ebf6b
annie0sc <annie0sc@gmail.com>
2020-11-20 00:49:21 -0600

    added recyclerView to the RegisterEventsActivity.java

--- app/src/main/java/com/example/event_expo/RegisterEventsActivity.java
+++ app/src/main/java/com/example/event_expo/RegisterEventsActivity.java
@@ -1,16 +1,65 @@
 package com.example.event_expo;
 
-//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
 
-public class RegisterEventsActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.event_expo.adapter.EventAdapter;
+import com.example.event_expo.listener.OnEventClickListener;
+import com.example.event_expo.model.Event;
+import com.example.event_expo.model.RequestedEvent;
+import com.example.event_expo.other.Constants;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+public class RegisterEventsActivity extends AppCompatActivity implements OnEventClickListener {
+
+    private RecyclerView recyclerView;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_events);
 
+        db = FirebaseFirestore.getInstance();
 
+        recyclerView = findViewById(R.id.rvRequestEvent);
+        fetchEvents();
+    }
+
+    private void fetchEvents() {
+        db.collection(Constants.requestedEvent)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            ArrayList<Event> events = new ArrayList<>();
+                            for (QueryDocumentSnapshot item : task.getResult()){
+                                events.add(item.toObject(RequestedEvent.class).getEvent());
+                            }
+                            EventAdapter adapter = new EventAdapter(events,RegisterEventsActivity.this);
+                            recyclerView.setAdapter(adapter);
+                        }else{
+                            Toast.makeText(RegisterEventsActivity.this, getString(R.string.please_try_again), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+    }
+
+    @Override
+    public void onEventClick(Event event,int position) {
+        // DO NOTHING
     }
 }
